7e18aef5703cce5197f02ddbcf8c274991b83ca8
songül kızılay <44816982+SongulKizilay@users.noreply.github.com>
2021-03-30 12:33:40 +0300

    Update 18-hangman.py

--- 18-hangman.py
+++ 18-hangman.py
@@ -9,43 +9,51 @@ print("""
                    |___/                               
 """)
 name = input("Adınızı giriniz :")
-print("hangman oyununa hoş geldin", name)
-print("\n"
-      "  +---+\n"
-      "  |   |\n"
-      "  O   |\n"
-      " /|\  |\n"
-      " / \  |\n"
-      "      |\n"
-     "=========''']\n")
-
-
-kelimeler  = ["karınca", "babun", "porsuk", "yarasa", "ayı", "kunduz", "deve", "kedi", "aslan", "kobra", "puma", "çakal" ,"karga",  "geyik", "köpek"]
-
-my_string = random.choice(kelimeler)
-sayi=len(my_string)
-giris_hakki =sayi+1
-
-while giris_hakki >0:
-    new_list = []
-    for element in my_string:
-        new_list.append(element)
-    harfgiris = input("bir harf giriniz")
-    if harfgiris in new_list:
-        print("bu harf var",harfgiris)
-        print(new_list.index(harfgiris))
-        print(f"{giris_hakki} giriş hakkınız var")
-        new_list.remove(harfgiris)
-    elif harfgiris == my_string:
-        print("kazandınız ")
-        print(f"gizli kelime {my_string}dir")
-
+key = 1
+while key == 1:
+    soru = input("Yapmak istediğiniz işlemin numarasını girin 1 giriş (Çıkmak için q): ")
+    if soru == "q":
+        print("çıkılıyor")
+        key == 0
         break
-    else:
-        giris_hakki -= 1
-        print(f"{giris_hakki} giriş hakkınız var")
+    elif soru == "1":
+        print("hangman oyununa hoş geldin", name)
+        print("\n"
+              "  +---+\n"
+              "  |   |\n"
+              "  O   |\n"
+              " /|\  |\n"
+              " / \  |\n"
+              "      |\n"
+              "=========''']\n")
 
-        print("false")
-        if giris_hakki==0:
-            print("öldün")
-            print(f"gizli kelime {my_string}dir")
+        kelimeler = ["karınca", "babun", "porsuk", "yarasa", "ayı", "kunduz", "deve", "kedi", "aslan", "kobra", "puma",
+                     "çakal", "karga", "geyik", "köpek"]
+
+        my_string = random.choice(kelimeler)
+        sayi = len(my_string)
+        giris_hakki = sayi + 1
+
+        while giris_hakki > 0:
+            new_list = []
+            for element in my_string:
+                new_list.append(element)
+            harfgiris = input("bir harf giriniz")
+            if harfgiris in new_list:
+                print("bu harf var", harfgiris)
+                print(new_list.index(harfgiris))
+                print(f"{giris_hakki} giriş hakkınız var")
+                new_list.remove(harfgiris)
+            elif harfgiris == my_string:
+                print("kazandınız ")
+                print(f"gizli kelime {my_string}dir")
+
+                break
+            else:
+                giris_hakki -= 1
+                print(f"{giris_hakki} giriş hakkınız var")
+
+                print("false")
+                if giris_hakki == 0:
+                    print("öldün")
+                    print(f"gizli kelime {my_string}dir")
