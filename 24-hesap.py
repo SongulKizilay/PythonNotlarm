0a65b02dfd09727861b68d5f43580f86056f5813
songül kızılay <44816982+SongulKizilay@users.noreply.github.com>
2021-04-02 15:19:48 +0300

    Update 24-hesap.py

--- 24-hesap.py
+++ 24-hesap.py
@@ -8,8 +8,10 @@ def calc(x,y,ops):
     elif ops == "*":
         return (str(x) + "" + ops + str(y) + "=" + str(x * y))
     elif ops == "/":
-        return (str(x) + "" + ops + str(y) + "=" + str(x / y))
-x=int(input("please enter first number"))
-y=int(input("please enter second number"))
-ops=input("choose between +,-,*,/")
-print(calc(x,y,ops))
+
+     return (str(x) + "" + ops + str(y) + "=" + str(x / y))
+while True:
+    x=int(input("please enter first number"))
+    y=int(input("please enter second number"))
+    ops=input("choose between +,-,*,/")
+    print(calc(x,y,ops))
